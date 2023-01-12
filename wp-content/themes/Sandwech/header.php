5f7843b4199212ad7130956d5f2f219ab7910bb0
alessiomodonesi <alessiomodo2004@gmail.com>
2023-01-12 22:37:02 +0100

    per sta sera ho finito

--- wp-content/themes/Sandwech/header.php
+++ wp-content/themes/Sandwech/header.php
@@ -14,21 +14,21 @@
     <header class="sticky-top">
         <nav class="navbar navbar-expand-lg bg-primary">
             <div class="container-fluid">
-                <a class="navbar-brand mb-0 h1" href="http://localhost/datatables">Sandwech</a>
+                <a class="navbar-brand mb-0 h1" href="http://localhost/sandwech">Sandwech</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
-                            <a class="nav-link active" href="http://localhost/datatables/user">User</a>
+                            <a class="nav-link active" href="http://localhost/sandwech/user">User</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link active" href="http://localhost/datatables/product">Product</a>
+                            <a class="nav-link active" href="http://localhost/sandwech/product">Product</a>
                         </li>
 
                         <li class="nav-item">
-                            <a class="nav-link active" href="http://localhost/datatables/ingredient">Ingredient</a>
+                            <a class="nav-link active" href="http://localhost/sandwech/ingredient">Ingredient</a>
                         </li>
                     </ul>
                     <form class="d-flex" role="search">
