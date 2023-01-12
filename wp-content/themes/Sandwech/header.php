2efc7606ca548f87775a59600de69defc26256f0
alessiomodonesi <alessiomodo2004@gmail.com>
2023-01-12 16:30:08 +0100

    Update header.php

--- wp-content/themes/Sandwech/header.php
+++ wp-content/themes/Sandwech/header.php
@@ -5,8 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Sandwech</title>
-    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/default/jquery-3.6.0.min.js">
-    </script>
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/js/default/jquery-3.6.0.min.js"></script>
     <?php wp_head(); ?>
 </head>
 
