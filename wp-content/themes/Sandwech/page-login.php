d63db2bf803c2fc935ed47b48d59f2e9b0fec2e8
alessiomodonesi <alessiomodo2004@gmail.com>
2023-01-16 12:21:45 +0100

    sistemato profilo, manca front-end login (vedi commit successivo)

--- wp-content/themes/Sandwech/page-login.php
+++ wp-content/themes/Sandwech/page-login.php
@@ -1,9 +1,8 @@
 <?php
 
-session_start();
-
+get_header();
 include_once dirname(__FILE__) . '/php/login.php';
-
+session_start();
 $err = "";
 $loginErr = "";
 
@@ -14,63 +13,38 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             "password" => $_POST['password'],
         ];
 
-        if (login($data) == -1) {
+        if (login($data) == -1)
             $loginErr = "Email o password errata";
-        }
-    } else {
+    } else
         $err = "Campo richiesto";
-    }
 }
+
 ?>
 
-<!DOCTYPE html>
-<html>
-
-<head>
-    <title>Login</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
 <body>
-    <div class="login-container">
-        <div class="screen-1">
-            <img src="static/img/app_logo.png" id="logo">
-            <g transform="matrix(1.01 0 0 1.01 362.9 210.9)">
-                <circle style="stroke: rgb(0, 0, 0); stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255, 255, 255); fill-rule: nonzero; opacity: 1" cx="0" cy="0" r="40"></circle>
-            </g>
-            <g transform="matrix(0.92 0 0 0.92 318.5 286.5)">
-                <circle style="stroke: rgb(0, 0, 0); stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255, 255, 255); fill-rule: nonzero; opacity: 1" cx="0" cy="0" r="40"></circle>
-            </g>
-            <g transform="matrix(0.16 -0.12 0.49 0.66 290.57 243.57)">
-                <polygon style="stroke: rgb(0, 0, 0); stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255, 255, 255); fill-rule: nonzero; opacity: 1" points="-50,-50 -50,50 50,50 50,-50 "></polygon>
-            </g>
-            <g transform="matrix(0.16 0.1 -0.44 0.69 342.03 248.34)">
-                <polygon style="stroke: rgb(0, 0, 0); stroke-width: 0; stroke-dasharray: none; stroke-linecap: butt; stroke-dashoffset: 0; stroke-linejoin: miter; stroke-miterlimit: 4; fill: rgb(255, 255, 255); fill-rule: nonzero; opacity: 1" vector-effect="non-scaling-stroke" points="-50,-50 -50,50 50,50 50,-50 "></polygon>
-            </g>
-            <form method="post">
-                <div class="email">
-                    <label for="email">Email Address</label>
-                    <div class="sec-2">
-                        <ion-icon name="mail-outline"></ion-icon>
-                        <input type="email" name="email" placeholder="Username@gmail.com" />
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-4 text-center">
+                <form method="post">
+
+                    <div class="mb-3 email">
+                        <label for="email" class="form-label">Email Address</label>
+                        <input type="email" class="form-control" name="email" placeholder="username@gmail.com">
+                        <span class="error-msg"><?php echo $err ?></span>
                     </div>
-                    <span class="error-msg"><?php echo $err ?></span>
-                </div><br>
-                <div class="password">
-                    <label for="password">Password</label>
-                    <div class="sec-2">
-                        <ion-icon name="lock-closed-outline"></ion-icon>
-                        <input class="pas" type="password" name="password" placeholder="············" />
-                        <ion-icon class="show-hide" name="eye-outline"></ion-icon>
+
+                    <div class="mb-3 password">
+                        <label for="password" class="form-label">Password</label>
+                        <input type="password" class="form-control" name="password" placeholder="password">
+                        <span class="error-msg"><?php echo $err ?></span>
                     </div>
-                    <span class="error-msg"><?php echo $err ?></span>
-                </div><br>
-                <button class="login">Login</button>
-                <span class="error-login"><?php echo $loginErr ?></span>
-            </form>
-            <div class="footer"><span>Sign up</span><span>Forgot Password?</span></div>
+
+                    <button type="submit" class="btn btn-outline-dark login">Login</button>
+                    <span class="error-login"><?php echo $loginErr ?></span>
+
+                </form>
+            </div>
         </div>
     </div>
-</body>
 
-</html>
+    <?php get_footer(); ?>
