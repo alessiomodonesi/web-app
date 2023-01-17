2081676acab7c0efdb7b8e7f2824e5d7eceb56cf
alessiomodonesi <alessiomodo2004@gmail.com>
2023-01-17 11:46:37 +0100

    btn list

--- wp-content/themes/Sandwech/page-mng.php
+++ wp-content/themes/Sandwech/page-mng.php
@@ -9,19 +9,7 @@
     </div>
 
     <div class="row">
-        <div class="col-2">
-            <ul id="table-list">
-                <a class="btn btn-warning table-list-name" href="http://localhost/sandwech/allergen">allergen</a>
-                <a class="btn btn-warning table-list-name" href="http://localhost/sandwech/break">break</a>
-                <a class="btn btn-warning table-list-name" href="http://localhost/sandwech/class">class</a>
-                <a class="btn btn-warning table-list-name" href="http://localhost/sandwech/pickup">pickup</a>
-                <a class="btn btn-warning table-list-name" href="http://localhost/sandwech/pickup-break">pickup_break</a>
-                <a class="btn btn-warning table-list-name" href="http://localhost/sandwech/reset">reset</a>
-                <a class="btn btn-warning table-list-name" href="http://localhost/sandwech/status">status</a>
-                <a class="btn btn-warning table-list-name" href="http://localhost/sandwech/user">user</a>
-                <a class="btn btn-warning table-list-name" href="http://localhost/sandwech/user-class">user_class</a>
-            </ul>
-        </div>
+
         <div class="col-10"></div>
     </div>
 </div>
