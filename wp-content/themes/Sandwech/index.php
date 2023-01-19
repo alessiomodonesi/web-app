bc957c72661f540ff5c509c0ce2ab3c50eff70b4
alessiomodonesi <alessiomodo2004@gmail.com>
2023-01-19 20:20:03 +0100

    risolto bug

---
+++ wp-content/themes/Sandwech/index.php
@@ -0,0 +1,1 @@
+<?php require("front-page.php");
--- wp-content/themes/Sandwech/php/search.php
+++ wp-content/themes/Sandwech/php/search.php
@@ -1,8 +1,32 @@
 <?php
-$array_admin = array('allergen', 'break', 'class', 'ingredient','login', 'management' , 'offer', 'order', 'pickup-break', 'pickup', 'product-allergen', 'product-ingredient', 'product-offer', 'product-order', 'product-tag', 'product', 'reset', 'status', 'tag');
+$array_admin = array(
+    'allergen',
+    'break',
+    'class',
+    'home',
+    'ingredient',
+    'management',
+    'offer',
+    'order',
+    'pickup-break',
+    'pickup',
+    'product-allergen',
+    'product-ingredient',
+    'product-offer',
+    'product-order',
+    'product-tag',
+    'product',
+    'reset',
+    'status',
+    'tag'
+);
 
-function search_page($value){
+function search_page($value)
+{
     echo $value;
-    header("Location: http://localhost/sandwech/". $value."/");
+
+    if ($value == 'home')
+        header("Location: http://localhost/sandwech");
+    else
+        header("Location: http://localhost/sandwech/" . $value);
 }
-?>
