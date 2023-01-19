5af1d07d0524726896929a145fe88317e7a29021
mattia-gallinaro <gallinaromattia6@gmail.com>
2023-01-19 21:23:10 +0100

    continuato la ricerca della pagina tramite searchbar

--- wp-content/themes/Sandwech/php/search.php
+++ wp-content/themes/Sandwech/php/search.php
@@ -10,6 +10,7 @@ $array_admin = array(
     'order',
     'pickup-break',
     'pickup',
+    'nutritional-value',
     'product-allergen',
     'product-ingredient',
     'product-offer',
@@ -21,12 +22,47 @@ $array_admin = array(
     'tag'
 );
 
+function search_if_exists($value)
+{
+    global $array_admin;
+    if(in_array($array_admin, $value)){
+        echo 'found';
+        return true;
+    }
+    return false;
+}
+
+function search_substring($value){
+    global $array_admin;
+    $close_calls = array();
+    for ($i = 0; $i < sizeof($array_admin); $i++) {
+        if(strpos($array_admin[$i], $value) == true){
+            $close_calls[] = $array_admin[$i];
+        }
+    }
+    if(count($close_calls) == 0){
+        echo 'Does not exist';
+        return false;
+    }
+    else if(count($close_calls) == 1){
+        echo 'Found';
+        return true;
+    } else
+        return $close_calls;
+}
 function search_page($value)
 {
     echo $value;
 
-    if ($value == 'home')
-        header("Location: http://localhost/sandwech");
-    else
-        header("Location: http://localhost/sandwech/" . $value);
+    if (search_if_exists($value) == true) {
+        if ($value == 'home')
+            header("Location: http://localhost/sandwech");
+        else
+            header("Location: http://localhost/sandwech/" . $value);
+    }
+
+    $result = search_substring($value);
+    return $result;
 }
+
+
