847b2e43fda09c010e0e703df599fceb4e3826c3
MatteoRigolin <72465769+MatteoRigolin@users.noreply.github.com>
2023-01-20 21:33:13 +0100

    sistemata pagina home
    aggiunta una card con le info dell'utente che ha eseguito l'accesso

--- wp-content/themes/Sandwech/front-page.php
+++ wp-content/themes/Sandwech/front-page.php
@@ -12,7 +12,7 @@ $mng = "mng@gmail.com";
     <div class="container-fluid">
         <div class="row">
             <div class="col-12">
-                <h2 class="title text-center">Ciao, <?php echo $user[0]->surname; ?>!</h2>
+                <h2 class="title text-center">Ciao, <?php echo $user[0]->name; ?>!</h2>
                 <hr />
             </div>
         </div>
@@ -20,20 +20,39 @@ $mng = "mng@gmail.com";
         <div class="row">
             <div class="col-4"></div>
             <div class="col-4 text-center login-form">
-                <img src="http://localhost/sandwech/wp-content/themes/Sandwech/assets/img/logo.png" alt="logo" class="d-inline-block align-text-top logo">
-                <ul class="list-group">
-                    <li class="list-group-item">Name: <?php echo $user[0]->name; ?></li>
-                    <li class="list-group-item">Surname: <?php echo $user[0]->surname; ?></li>
-                    <li class="list-group-item">Email: <?php echo $user[0]->email; ?></li>
-                    <?php if ($user[0]->email == $admin) : ?>
-                        <li class="list-group-item">Role: admin</li>
-                    <?php elseif ($user[0]->email == $mng) : ?>
-                        <li class="list-group-item">Role: management</li>
-                    <?php elseif ($user[0]->email == $vendite) : ?>
-                        <li class="list-group-item">Role: vendite</li>
-                    <? endif ?>
-                </ul>
-                <a class="btn btn-outline-dark logout-btn" href="http://localhost/sandwech/wp-content/themes/Sandwech/php/login/logout.php" role="button">Logout</a>
+                <div class="card mx-auto">
+                    <img src="http://localhost/sandwech/wp-content/themes/Sandwech/assets/img/logo.png" alt="logo"
+                        class="d-inline-block align-text-top logo">
+                    <div class="container text-center">
+
+                        <?php if ($user[0]->email == $admin) : ?>
+                        <h4><b>Admin</b></h4>
+                        <p>Email: <?php echo $user[0]->email; ?></p>
+                        <p>Role: Administrator</p>
+
+                        <?php elseif ($user[0]->email == $mng) : ?>
+                        <h4><b>Management</b></h4>
+                        <p>Email: <?php echo $user[0]->email; ?></p>
+                        <p>Role: Accounts Management</p>
+
+                        <?php elseif ($user[0]->email == $vendite) : ?>
+                        <h4><b>Vendite</b></h4>
+                        <p>Email: <?php echo $user[0]->email; ?></p>
+                        <p>Role: Gestore Vendite</p>
+
+                        <?php else : ?>
+                        <h4><b><?php echo $user[0]->name; ?> <?php echo $user[0]->surname; ?></b></h4>
+                        <p>Email: <?php echo $user[0]->email; ?></p>
+                        <p>Role: User</p>
+
+                        <?php endif ?>
+
+                    </div>
+                </div>
+
+                <a class="btn btn-outline-dark logout-btn"
+                    href="http://localhost/sandwech/wp-content/themes/Sandwech/php/login/logout.php"
+                    role="button">Logout</a>
             </div>
             <div class="col-4">
             </div>
