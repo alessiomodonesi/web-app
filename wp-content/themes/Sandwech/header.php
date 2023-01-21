8f3d08ba0e0ea79ab0959240eec7c44fa6274561
MatteoRigolin <72465769+MatteoRigolin@users.noreply.github.com>
2023-01-21 16:00:29 +0100

    ora il collapse del bottone funziona

--- wp-content/themes/Sandwech/header.php
+++ wp-content/themes/Sandwech/header.php
@@ -45,7 +45,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <a class="nav-link active" href="http://localhost/sandwech">Home</a>
