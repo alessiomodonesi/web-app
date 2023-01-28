b6eb84d4b2fe640c8c59a32ebc4fe2401e25fbf8
alessiomodonesi <alessiomodo2004@gmail.com>
2023-01-28 08:39:22 +0100

    cambiato editor

--- EditorPHP/lib/config.php
+++ EditorPHP/lib/config.php
@@ -1,5 +1,4 @@
-<?php if (!defined('DATATABLES'))
-	exit(); // Ensure being used in DataTables env.
+<?php if (!defined('DATATABLES')) exit(); // Ensure being used in DataTables env.
 
 // Enable error reporting for debugging (remove for production)
 error_reporting(E_ALL);
@@ -10,21 +9,14 @@ ini_set('display_errors', '1');
  * Database user / pass
  */
 $sql_details = array(
-	"type" => "Mysql",
-	// Database type: "Mysql", "Postgres", "Sqlserver", "Sqlite" or "Oracle"
-	"user" => "root",
-	// Database user name
-	"pass" => "",
-	// Database password
-	"host" => "localhost",
-	// Database host
-	"port" => "",
-	// Database connection port (can be left empty for default)
-	"db" => "sandwiches",
-	// Database name
-	"dsn" => "",
-	// PHP DSN extra information. Set as `charset=utf8mb4` if you are using MySQL
-	"pdoAttr" => array() // PHP PDO attributes array. See the PHP documentation for all options
+	"type" => "Mysql",     // Database type: "Mysql", "Postgres", "Sqlserver", "Sqlite" or "Oracle"
+	"user" => "root",          // Database user name
+	"pass" => "",          // Database password
+	"host" => "localhost", // Database host
+	"port" => "3306",          // Database connection port (can be left empty for default)
+	"db"   => "sandwiches",          // Database name
+	"dsn"  => "",          // PHP DSN extra information. Set as `charset=utf8mb4` if you are using MySQL
+	"pdoAttr" => array()   // PHP PDO attributes array. See the PHP documentation for all options
 );
 
 
