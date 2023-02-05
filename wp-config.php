ea4ba84c95c8f4dddcfb2117133198cfc92213a8
alessiomodonesi <alessiomodo204@gmail.com>
2023-02-05 16:04:40 +0100

    test

--- wp-config.php
+++ wp-config.php
@@ -30,7 +30,7 @@ define('DB_USER', 'root');
 define('DB_PASSWORD', '');
 
 /** Hostname del database */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'sandwech.ddns.net');
 
 /** Charset del Database da utilizzare nella creazione delle tabelle. */
 define('DB_CHARSET', 'utf8mb4');
