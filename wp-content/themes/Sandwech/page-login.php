9fe86df04f028815348c51d2f23365be961599ed
alessiomodonesi <alessiomodo2004@gmail.com>
2023-02-15 16:46:43 +0100

    Update page-login.php

--- wp-content/themes/Sandwech/page-login.php
+++ wp-content/themes/Sandwech/page-login.php
@@ -57,7 +57,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                         </li>
                     </ul>
                     <form class="d-flex" role="search">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" disabled>
                         <button class="btn btn-outline-light search-btn" type="submit">Search</button>
                     </form>
                 </div>
