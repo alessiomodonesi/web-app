50f3192ee42ad99006533e41a97a6c19b7e9fdab
alessiomodonesi <alessiomodo2004@gmail.com>
2023-02-19 20:26:49 +0100

    .

--- wp-content/themes/Sandwech/php/btn-tag.php
+++ wp-content/themes/Sandwech/php/btn-tag.php
@@ -1,31 +1,26 @@
-<?php include_once dirname(__FILE__) . '/product/getProduct.php'; ?>
-
 <script type="text/javascript">
+    async function getProduct(id) {
+        const response = await fetch("http://localhost/food-api/API/product/getProduct.php?PRODUCT_ID=" + id)
+            .then((response) => response.json())
+            .then((data) => {
+                console.log(data);
+                return data;
+            });
+    }
+
     $(window).on('load', function() {
         $('#<?php echo strtolower(get_the_title()) ?> tbody').on('click', 'tr', function() {
             var id = $(this).attr("id").split("_");
-
-            $.ajax({
-                url: "../wp-content/themes/Sandwech/php/product/getProduct.php",
-                method: "POST",
-                data: {
-                    id: id[1]
-                },
-                success: function(response) {
-                    console.log(response);
-                }
-            });
+            var product = getProduct(id[1]);
         });
     });
 </script>
-
-<?php $product = getProduct(); ?>
 
 <div class="row">
     <div class="position-relative">
         <div class="btn-group position-absolute top-50 start-50 translate-middle" role="group">
             <a class="btn btn-outline-dark btn-cart" href="#" role="button"> - </a>
-            <a class="btn btn-outline-dark btn-quantity" role="button"><?php echo $quantity; ?></a>
+            <a class="btn btn-outline-dark btn-quantity" role="button"> 3 </a>
             <a class="btn btn-outline-dark btn-cart" href="#" role="button"> + </a>
             <a class="btn btn-outline-dark btn-cart" href="#" role="button">
                 <img src="http://localhost/sandwech/wp-content/themes/Sandwech/assets/img/trash.png" alt="home" width="25" height="25" class="d-inline-block home-png">
--- wp-content/themes/Sandwech/php/product/getProduct.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function getProduct()
-{
-    $url = 'http://localhost/food-api/API/product/getProduct.php?PRODUCT_ID=' . $_POST["id"];
-    return json_decode(file_get_contents($url));
-}
